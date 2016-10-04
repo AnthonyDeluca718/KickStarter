714d39c2c8edfdbde2999cf03205b11e01d966c7
Anthony <adeluca@gmx.com>
2016-10-04 17:50:46 -0400

    update production.rb for assets on heroku

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'bcrypt'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # config/environments/production.rb
+  config.serve_static_files = true
+  # ...
+  config.log_level = :debug
 end
