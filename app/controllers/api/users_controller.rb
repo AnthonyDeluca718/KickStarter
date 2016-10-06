fd4dfe2b47ca2a2e316fabfe2a24a4d28795aa44
Anthony <adeluca@gmx.com>
2016-10-06 10:34:52 -0400

    custum photo urls work

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,7 +14,7 @@ class Api::UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:name, :password)
+		params.require(:user).permit(:name, :password, :photo_url)
 	end
 
 end
