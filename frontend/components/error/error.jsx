4870482e3cd433f7d4490edc9b0bfeb46b2f9f9a
Anthony <adeluca@gmx.com>
2016-10-08 15:41:29 -0400

    Did alot of styling. Site looks better.

--- frontend/components/error/error.jsx
+++ frontend/components/error/error.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import {Link, hashHistory} from 'react-router';
 
 class Error extends React.Component {
   render() {
     return(
-      <h3>This isn't the page you are looking for!</h3>
+      <div className="error">
+
+        <div className="logo">
+          <Link to="/" className="profile-cloned">CLONED</Link>
+          <Link to="/" className="profile-starter">STARTER</Link>
+        </div>
+
+        <div className="content">
+          <h1>404</h1>
+          <h2 className="ohMy">OH MY GOODNESS</h2>
+            <div className="message">
+              We apologize but something's gone wrong&#45; an old link, a bad link, or some little glitch.
+            </div>
+            <Link to="/" className="error-page-link">Go to home page</Link>
+          </div>
+      </div>
     );
   }
 }
