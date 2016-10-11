160312ff16c66ed25bcbc52aa8f9cee4727a5e4a
Anthony <adeluca@gmx.com>
2016-10-11 11:36:30 -0400

    bug fix for search

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -3,7 +3,6 @@ import {Link, hashHistory} from 'react-router';
 
 class Search extends React.Component{
   constructor(props){
-
     super(props);
     this.state = {data: ""};
     this.viewProject = this.viewProject.bind(this);
@@ -26,26 +25,36 @@ class Search extends React.Component{
 
   render (){
     const {search} = this.props;
-    let results = search.map((project)=>{return(
-                    <p key={project.id}>
-                      <Link to={`/projects/${project.id}`} className="search-element">{project.title}</Link>
-                    </p>
-                  );});
+
+    let results;
+    if (this.state.data.length > 0) {
+      results = search.map((project)=>{return(
+        <p key={project.id}>
+          <Link to={`/projects/${project.id}`} className="search-element">{project.title}</Link>
+        </p>
+      );});
+    } else {
+      results = <div className="search-description">Search is by creator name and project title.</div>
+    }
+    console.log(this.state.data);
+
+    let renderRes;
+
     return(
       <div className="search-bar">
+        <input className="search-bar-input"
+          type="text"
+          value={this.state.data}
+          onChange={this.getSearch(this.state.data)}
+          placeholder="Search"
+          ></input>
 
-          <input className="search-bar-input"
-            type="text"
-            value={this.state.data}
-            onChange={this.getSearch(this.state.data)}
-            placeholder="Search"
-            ></input>
-
-          <div className="results">
-            {results}
-          </div>
+        <div className="results">
+          {results}
+        </div>
       </div>
     );
+
   }
 }
 
