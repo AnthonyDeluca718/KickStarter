4dea3b01402134efd365c6133d9c6e8d69c424ee
Anthony <adeluca@gmx.com>
2016-10-30 19:18:30 -0400

    resume is loaded on the page nicely

--- frontend/reducers/search_reducer.js
+++ frontend/reducers/search_reducer.js
@@ -1,12 +1,12 @@
 import { RECEIVE_SEARCH } from '../actions/search_actions';
 
 const SearchReducer = (state = [], action) => {
-    switch(action.type){
-      case RECEIVE_SEARCH:
-        return action.search;
-      default:
-        return state;
-    }
-  };
+  switch(action.type){
+    case RECEIVE_SEARCH:
+      return action.search;
+    default:
+      return state;
+  }
+};
 
-  export default SearchReducer;
+export default SearchReducer;
